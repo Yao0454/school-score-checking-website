3db4d27ec8eb2325aee21de060b71cd6421faff1
枫枫子. <81960779+Yao0454@users.noreply.github.com>
2024-08-30 02:23:16 +0800

    Catch.py

--- Catch.py
+++ Catch.py
@@ -5,13 +5,14 @@ import os
 import time
 
 # 读取Excel表格
-df = pd.read_excel(r"C:\Users\yaoyi\Desktop\Grade\21.xlsx")
+df = pd.read_excel("你的班级或学校所对应的学生信息文档 默认excel表格")
 
 # 初始化WebDriver（Edge浏览器）
 driver = webdriver.Edge()
+# 按需修改浏览器
 
 # 打开指定的网页
-driver.get("https://i0aew1vd.yichafen.com/qz/c7h7ZvbhOt")
+driver.get("你的查分网站")
 
 # 存储抓取到的数据
 output_data = []
@@ -24,11 +25,13 @@ for index, row in df.iterrows():
 
     data1 = row[1]  # 从 Excel 表格中读取第二列的值
     data2 = str(row[2])[-4:]  # 从 Excel 表格中读取第三列的后四位
+    #data1对应姓名 data2对应查询码
     
     # 定位并输入数据到网页的输入框中
     input1 = driver.find_element(By.NAME, 's_xingming')  # 定位第一个输入框
     input2 = driver.find_element(By.NAME, 's_chaxunma')  # 定位第二个输入框
-
+    # 自己找网站的输入位置
+    
     input1.clear()  # 清除第一个输入框中的内容
     input2.clear()  # 清除第二个输入框中的内容
     
@@ -38,10 +41,12 @@ for index, row in df.iterrows():
     # 定位带有“查询”字样的按钮并点击
     submit_button = driver.find_element(By.XPATH, '//button[contains(text(),"查询")]')
     submit_button.click()
-
+    # 按需修改按钮字样
+    
     # 等待页面加载
     time.sleep(0.5)
-
+    # 按需修改时间
+    
     # 抓取输出并保存
     elements = driver.find_elements(By.CLASS_NAME, 'right_cell')  # 查找所有包含输出数据的元素
     output_values = [element.text for element in elements]  # 提取所有元素的文本
